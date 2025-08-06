782976d8d955b443b27ded7defb2ff51bce03b10
Mariana Caradec <maricaradec@gmail.com>
2025-08-06 17:07:57 -0300

    feat: created draft of NavBar model

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavBar = () => {
+  const router = usePathname();
+  const isHomePage = router === "/";
+
+  return (
+    <nav
+      className={`flex items-center justify-between px-6 py-4 w-full ${
+        isHomePage
+          ? "absolute z-20 text-[#CFAC91]"
+          : "relative bg-white text-[#AF611E] shadow-md"
+      }`}
+    >
+      <div>
+        <Link
+          href="/"
+          className="text-2xl text-[#CFAC91] p-2 rounded-lg font-bold"
+        >
+          Texere
+        </Link>
+      </div>
+
+      <div className="flex items-center space-x-6">
+        <ul className="md:flex items-center justify-around">
+          <li className="hover:text-[#AF611E] hover:bg-[#CFAC91] p-2 rounded-lg font-bold transition-color duration-200 cursor-pointer">
+            Sobre nosotros
+          </li>
+          <li className="hover:text-[#AF611E] hover:bg-[#CFAC91] p-2 rounded-lg font-bold transition-color duration-200 cursor-pointer">
+            Libros
+          </li>
+          <li className="hover:text-[#AF611E] hover:bg-[#CFAC91] p-2 rounded-lg font-bold transition-color duration-200 cursor-pointer">
+            Reseñas
+          </li>
+          <li className="hover:text-[#AF611E] hover:bg-[#CFAC91] p-2 rounded-lg font-bold transition-color duration-200 cursor-pointer">
+            Contacto
+          </li>
+          <li className="hover:text-[#AF611E] hover:bg-[#CFAC91] p-2 rounded-lg font-bold transition-color duration-200 cursor-pointer">
+            Envios
+          </li>
+        </ul>
+
+        <div className="flex items-center space-x-2">
+          <button className="hover:bg-[#AF611E] rounded-lg p-2 transition-color duration-200 cursor-pointer">
+            {" "}
+            <Link href="/ingresar">Ingresar</Link>{" "}
+          </button>
+          <button className="hover:bg-[#AF611E] rounded-lg p-2 transition-color duration-200 cursor-pointer">
+            <Link href="/cart">Carrito</Link>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
