f83346a5bd357392c60fdd796d479ec678bcc28b
Mariana Caradec <maricaradec@gmail.com>
2025-08-07 18:03:39 -0300

    feat: added ReseñasEnviosContacto component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
 import LibrosSeleccionados from "./components/LibrosSeleccionados";
+import ReseñasEnviosContacto from "./components/ReseñasEnviosContacto";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen text-[#CFAC91]">
-      <Image
-        src="/about-us2.png"
-        alt="About Us"
-        layout="fill"
-        objectFit="cover"
-        className="absolute inset-0 -z-10"
-      />
-      <div className="absolute inset-0 bg-black/50"></div>
+    <div>
+      <div className="relative min-h-screen text-[#CFAC91]">
+        <Image
+          src="/about-us2.png"
+          alt="About Us"
+          layout="fill"
+          objectFit="cover"
+          className="absolute inset-0 -z-10"
+        />
+        <div className="absolute inset-0 bg-black/50"></div>
 
-      <div className="flex-grow flex items-center justify-center p-8 relative z-10 drop-shadow-[2px_2px_0px_#000]">
-        <section className="flex flex-col justify-center items-center h-screen text-center">
-          <h1 className="text-4xl md:text-6xl">
-            ¡Te damos la bienvenida a Texere Libros!
-          </h1>
-          <h2 className="text-lg md:text-2xl mt-4">
-            El lugar donde tu imaginación cobra vida a través de las palabras.
-          </h2>
-          <h3 className="text-base md:text-xl mt-7">
-            Explora nuestra colección de libros, descubre reseñas y sumérgete en
-            un mundo de historias cautivadoras a un click de distancia.
-          </h3>
-          <Link
-            href="/sobreNosotros"
-            className="mt-6 px-6 py-3 bg-[#AF611E] hover:bg-[#CFAC91] hover:text-[#AF611E] transition rounded-lg"
-          >
-            Descubre más sobre nosotros
-          </Link>
-        </section>
+        <div className="flex-grow flex items-center justify-center p-8 relative z-10 drop-shadow-[2px_2px_0px_#000]">
+          <section className="flex flex-col justify-center items-center h-screen text-center">
+            <h1 className="text-4xl md:text-6xl">
+              ¡Te damos la bienvenida a Texere Libros!
+            </h1>
+            <h2 className="text-lg md:text-2xl mt-4">
+              El lugar donde tu imaginación cobra vida a través de las palabras.
+            </h2>
+            <h3 className="text-base md:text-xl mt-7">
+              Explora nuestra colección de libros, descubre reseñas y sumérgete
+              en un mundo de historias cautivadoras a un click de distancia.
+            </h3>
+            <Link
+              href="/sobreNosotros"
+              className="mt-6 px-6 py-3 bg-[#AF611E] hover:bg-[#CFAC91] hover:text-[#AF611E] transition rounded-lg"
+            >
+              Descubre más sobre nosotros
+            </Link>
+          </section>
+        </div>
       </div>
+
       <div className="flex-grow flex items-center justify-center p-8 relative z-10 bg-white bg-opacity-80">
         <section>
           <LibrosSeleccionados />
+        </section>
+      </div>
+      <div className="flex-grow flex items-center justify-center p-8 relative z-10 bg-[#BF9A7C] bg-opacity-80">
+        <section>
+          <ReseñasEnviosContacto />
         </section>
       </div>
       {/* <footer className="flex flex-wrap items-center justify-center relative z-20 gap-6">
