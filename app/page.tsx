59fef1dbd925ca8026b86a00dc55fdd98e842203
Mariana Caradec <maricaradec@gmail.com>
2025-08-11 18:02:36 -0300

    style: added footer to layout and removed it from home page -added font serif too

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ReseñasEnviosContacto from "./components/ReseñasEnviosContacto";
 
 export default function Home() {
   return (
-    <div>
+    <div className="font-serif">
       <div className="relative min-h-screen text-[#CFAC91]">
         <Image
           src="/about-us2.png"
@@ -43,58 +43,11 @@ export default function Home() {
           <LibrosSeleccionados />
         </section>
       </div>
-      <div className="flex-grow flex items-center justify-center p-8 relative z-10 bg-[#BF9A7C] bg-opacity-80">
+      <div className="flex-grow flex items-center justify-center p-8 relative z-10 bg-gradient-to-b from-[#BF9A7C] via-[#BF9A7C]/80 to-transparent">
         <section>
           <ReseñasEnviosContacto />
         </section>
       </div>
-      {/* <footer className="flex flex-wrap items-center justify-center relative z-20 gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
     </div>
   );
 }
